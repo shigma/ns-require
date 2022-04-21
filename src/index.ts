5198a141d8ec0ec7e1d9d02e07682bb939573a1b
Shigma <shigma10826@gmail.com>
2022-04-21 15:52:50 +0800

    feat: export typings

--- src/index.ts
+++ src/index.ts
@@ -1,88 +1,89 @@
 import { isAbsolute, resolve } from 'path'
 
-namespace Scope {
+function ns(options: ns.Options) {
+  return new ns.Scope(options)
+}
+
+namespace ns {
+  export function unwrapExports(module: any) {
+    return module?.default || module
+  }
+
   export interface Options {
     namespace: string
     prefix: string
     official?: string
     dirname?: string
   }
-}
 
-class Scope {
-  private prefixes: string[]
+  export class Scope {
+    private prefixes: string[]
 
-  constructor(public options: Scope.Options) {
-    this.prefixes = [`${options.namespace}-${options.prefix}-`]
-    if (options.official) {
-      this.prefixes.push(`@${options.official}/${options.prefix}-`)
-    }
-  }
-
-  private throwError(name: string): never {
-    throw new Error(`cannot resolve ${this.options.prefix} "${name}"`)
-  }
-
-  paths(name: string) {
-    // absolute path
-    if (isAbsolute(name)) {
-      return [name]
-    }
-
-    // relative path
-    if (name.startsWith('./') || name.startsWith('../')) {
-      if (this.options.dirname) {
-        return [resolve(this.options.dirname, name)]
-      } else {
-        this.throwError(name)
+    constructor(public options: Options) {
+      this.prefixes = [`${options.namespace}-${options.prefix}-`]
+      if (options.official) {
+        this.prefixes.push(`@${options.official}/${options.prefix}-`)
       }
     }
 
-    // full package path
-    if (this.prefixes.some(prefix => name.startsWith(prefix))) {
-      return [name]
+    private throwError(name: string): never {
+      throw new Error(`cannot resolve ${this.options.prefix} "${name}"`)
     }
 
-    // scoped package path
-    if (name[0] === '@') {
-      const index = name.indexOf('/')
-      if (index < 0) this.throwError(name)
-      const scope = name.slice(0, index + 1)
-      name = name.slice(index + 1)
-      if (!name.startsWith(this.prefixes[0])) {
-        name = this.prefixes[0] + name
+    paths(name: string) {
+      // absolute path
+      if (isAbsolute(name)) {
+        return [name]
       }
-      return [scope + name]
+
+      // relative path
+      if (name.startsWith('./') || name.startsWith('../')) {
+        if (this.options.dirname) {
+          return [resolve(this.options.dirname, name)]
+        } else {
+          this.throwError(name)
+        }
+      }
+
+      // full package path
+      if (this.prefixes.some(prefix => name.startsWith(prefix))) {
+        return [name]
+      }
+
+      // scoped package path
+      if (name[0] === '@') {
+        const index = name.indexOf('/')
+        if (index < 0) this.throwError(name)
+        const scope = name.slice(0, index + 1)
+        name = name.slice(index + 1)
+        if (!name.startsWith(this.prefixes[0])) {
+          name = this.prefixes[0] + name
+        }
+        return [scope + name]
+      }
+
+      // normal package path
+      return this.prefixes.map(prefix => prefix + name).reverse()
     }
 
-    // normal package path
-    return this.prefixes.map(prefix => prefix + name).reverse()
-  }
-
-  require(name: string) {
-    const path = this.resolve(name)
-    const exports = require(path)
-    return ns.unwrapExports(exports)
-  }
-
-  resolve(name: string) {
-    const modules = this.paths(name)
-    for (const path of modules) {
-      try {
-        return require.resolve(path)
-      } catch {}
+    require(name: string) {
+      if (typeof name === 'object') {
+        return name
+      }
+      const path = this.resolve(name)
+      const exports = require(path)
+      return ns.unwrapExports(exports)
     }
-    this.throwError(name)
-  }
-}
 
-function ns(options: Scope.Options) {
-  return new Scope(options)
-}
-
-namespace ns {
-  export function unwrapExports(module: any) {
-    return module?.default || module
+    resolve(name: string) {
+      const modules = this.paths(name)
+      for (const path of modules) {
+        try {
+          return require.resolve(path)
+        } catch {}
+      }
+      this.throwError(name)
+    }
   }
 }
 
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -15,16 +15,16 @@ describe('ns-require', () => {
   })
 
   it('with official scope', () => {
-    const scope = ns({
+    const require = ns({
       namespace: 'koishi',
       prefix: 'plugin',
       official: 'koishijs',
     })
 
-    expect(scope.paths('foo-bar')).to.deep.equal(['@koishijs/plugin-foo-bar', 'koishi-plugin-foo-bar'])
-    expect(scope.paths('koishi-plugin-foo-bar')).to.deep.equal(['koishi-plugin-foo-bar'])
-    expect(scope.paths('@koishijs/plugin-foo-bar')).to.deep.equal(['@koishijs/plugin-foo-bar'])
-    expect(scope.paths('@qux/foo-bar')).to.deep.equal(['@qux/koishi-plugin-foo-bar'])
-    expect(scope.paths('@qux/koishi-plugin-foo-bar')).to.deep.equal(['@qux/koishi-plugin-foo-bar'])
+    expect(require.paths('foo-bar')).to.deep.equal(['@koishijs/plugin-foo-bar', 'koishi-plugin-foo-bar'])
+    expect(require.paths('koishi-plugin-foo-bar')).to.deep.equal(['koishi-plugin-foo-bar'])
+    expect(require.paths('@koishijs/plugin-foo-bar')).to.deep.equal(['@koishijs/plugin-foo-bar'])
+    expect(require.paths('@qux/foo-bar')).to.deep.equal(['@qux/koishi-plugin-foo-bar'])
+    expect(require.paths('@qux/koishi-plugin-foo-bar')).to.deep.equal(['@qux/koishi-plugin-foo-bar'])
   })
 })
